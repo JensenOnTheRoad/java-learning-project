b372c54cb83946975b035930a775f95953648a12
jensen_deng <jensendengwork@163.com>
2023-02-10 02:57:37 +0800

    :sparkles: Add common dependencies

---
+++ java-8-based/src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jensen_deng
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+
+  @Test
+  void test() {
+    System.out.println("hello world");
+  }
+
+}
