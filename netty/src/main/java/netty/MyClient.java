cd34f77d14db0c1ed45adbf277ec9149821a9665
jensen_deng <jensendengwork@163.ocm>
2023-05-31 22:35:13 +0800

    :sparkles: netty retry

--- netty/src/main/java/netty/MyClient.java
+++ netty/src/main/java/netty/MyClient.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -14,8 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MyClient {
 
+  public static final int MAX_RETRY = 5;
+
   public static void main(String[] args) throws Exception {
     int clientPort = 6666;
+    int serverPort = 8000;
 
     NioEventLoopGroup eventExecutors = new NioEventLoopGroup();
     try {
@@ -39,9 +44,10 @@ public class MyClient {
           .handler(handler);
       log.info("客户端准备就绪，随时可以起飞~");
       // 连接服务端
+      String host = "127.0.0.1";
       ChannelFuture channelFuture =
           bootstrap
-              .connect("127.0.0.1", clientPort)
+              .connect(host, clientPort)
               .addListener(
                   future -> {
                     if (future.isSuccess()) {
@@ -49,6 +55,7 @@ public class MyClient {
                     } else {
                       log.info("Connection failure!");
                       // 重新连接
+                      connect(bootstrap, host, serverPort, MAX_RETRY);
                     }
                   })
               .sync();
@@ -59,5 +66,29 @@ public class MyClient {
       // 关闭线程组
       eventExecutors.shutdownGracefully();
     }
+  }
+
+  private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+    bootstrap
+        .connect(host, port)
+        .addListener(
+            future -> {
+              if (future.isSuccess()) {
+                log.info("连接成功!");
+              } else if (retry == 0) {
+                log.info("重试次数已用完，放弃连接！");
+              } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔，1<<order相当于1乘以2的order次方
+                int delay = 1 << order;
+                log.info(LocalDateTime.now() + ": 连接失败，第" + order + "次重连……");
+                bootstrap
+                    .config()
+                    .group()
+                    .schedule(
+                        () -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+              }
+            });
   }
 }
